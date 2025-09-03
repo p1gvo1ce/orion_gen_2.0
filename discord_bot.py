0d2d541c9bf97b7a9b071a34a86055c27e27fe5f
p1gvo1ce <gaussga@mail.ru>
2025-09-03 22:19:08 +0300

    донастроил логи, чтобы не было "невызываемых" логов. изменил запуск экземпляров бота на асинк таски чтобы можно было по аналогии добавлять экземпляры других платформ.

--- discord_bot.py
+++ discord_bot.py
@@ -5,9 +5,13 @@ from utils.logger import get_logger, set_process
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-async def run_bot(token: str):
+@bot.event
+async def on_ready():
+    log = get_logger(module="discord_bot")
+    log.INFO(msg=f"✅ Bot connected as {bot.user} (id={bot.user.id})")
+
+async def run_bot(token: str, event_id=None):
     log = get_logger(module="discord_bot")
     set_process("discord_bot")            # тот же event_id, новый процесс
-    log.INFO("Starting bot...")
+    log.INFO(msg="Starting bot...", event_id=event_id)
     await bot.start(token)
-    log.INFO("Bot started")
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.logger import (
     begin_event, set_process,
     hook_std_logging
 )
-from discord_bot import run_bot
+from discord_bot import run_bot as run_discord
 
 class Settings(BaseSettings):
     discord_token: str
@@ -17,31 +17,35 @@ class Settings(BaseSettings):
 settings = Settings()
 
 async def main():
-    # 0) база + фоновый воркер логов
+    # 0) база + воркер
     await init_db()
     asyncio.create_task(sql_log_writer())
 
     # 1) глобальный логгер
     init_global_logger(source="orion", version="0.1", log_level="INFO")
+
+    # 2) подключить stdlib logging → наш логгер
     hook_std_logging(quiet_access=True)
+
+    # 3) взять фасад
     log = get_logger(module="main")
 
-    # 2) старт общей цепочки запуска сервиса
+    # 4) старт цепочки
     event_id = begin_event(process="main")
     log.INFO("🚀 Orion startup begin", event_id=event_id)
 
-    try:
-        # 3) этап «инициализация дискорда» — тот же event_id, другой process
-        set_process("discord_bootstrap")
-        log.INFO("🤖 Discord bot init...", event_id=event_id)
+    # старт общей цепочки
+    event_id = begin_event(process="main")
+    log.INFO("🚀 Orion startup begin", event_id=event_id)
 
-        await run_bot(token=settings.discord_token)  # внутри доступен текущий контекст
+    # таски для разных платформ
+    tasks = []
+    tasks.append(asyncio.create_task(run_discord( token=settings.discord_token, event_id=event_id)))
+    log.DEBUG(msg="Orion Discord startup", event_id=event_id)
 
-        # 4) вернёмся в 'main' и финализируем
-        set_process("main")
-        log.INFO("✅ Orion startup success", event_id=event_id)
-    except Exception as e:
-        log.EXCEPTION("❌ Orion startup failed", e, event_id=event_id)
+
+    # подождём, пока они все живы
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
